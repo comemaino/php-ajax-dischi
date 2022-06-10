40485df0e955bb7348c361090e443486c5f11ae9
comemaino <mat.maino@gmail.com>
2022-06-10 16:54:37 +0200

    Add: milestone 2

--- index-json.php
+++ index-json.php
@@ -5,10 +5,12 @@
   <meta http-equiv="X-UA-Compatible" content="IE=edge">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <title>Document</title>
-      <!-- Axios -->
-      <script src="https://cdnjs.cloudflare.com/ajax/libs/axios/0.27.2/axios.min.js" integrity="sha512-odNmoc1XJy5x1TMVMdC7EMs3IVdItLPlCeL5vSUPN2llYKMJ2eByTTAIiiuqLg+GdNr9hF6z81p27DArRFKT7A==" crossorigin="anonymous" referrerpolicy="no-referrer"></script>
+    <!-- Axios -->
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/axios/0.27.2/axios.min.js" integrity="sha512-odNmoc1XJy5x1TMVMdC7EMs3IVdItLPlCeL5vSUPN2llYKMJ2eByTTAIiiuqLg+GdNr9hF6z81p27DArRFKT7A==" crossorigin="anonymous" referrerpolicy="no-referrer"></script>
     <!-- VueJs -->
     <script src="https://cdn.jsdelivr.net/npm/vue@2.6.14/dist/vue.js"></script>
+    <!-- srtyle -->
+    <link rel="stylesheet" href="css/style.css">
 </head>
 <body>
 <main id="root">
@@ -19,6 +21,7 @@
   <img :src="disc.poster" alt="">
   <h3>{{ disc.title }}</h3>
   <h5>{{ disc.author }}</h5>
+  <h5>{{ disc.year }}</h5>
 </div>
 </div>
 </main>
